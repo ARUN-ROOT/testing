befb37a5fde33621d9ec6ee8c27a833f737a7163
Arun <51967303+ARUN-ROOT@users.noreply.github.com>
2023-05-10 11:50:13 +0530

    ipaddress concept added

--- 02_client.c
+++ 02_client.c
@@ -6,6 +6,14 @@
 * Output	:
 */
 
+/*
+* Name		: 
+* Date		: 22/4/23			
+* Author	: 
+* Descrption	:
+* Output	:
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -15,10 +23,26 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#include <fcntl.h>
+#include <sys/stat.h>
+
+
 
 int main()
 {
+  int fd,flag,mode;
+  ssize_t numread;
+  char arr[1024];
+ 
+  flag=O_CREAT | O_RDWR | O_APPEND;
+  mode = S_IRWXU | S_IRWXG | S_IRWXO ;
+  
+  fd=open("ipaddress.txt",flag,mode); //opening here
+  perror("open()");
 
+ numread =read(fd,arr,1024); //reading and store arr
+ 
+  ////==================================================================/////
   int sock_fd,c_fd;
   sock_fd=socket(AF_INET,SOCK_STREAM,0);
   
@@ -26,8 +50,11 @@ int main()
   serveraddress.sin_family=AF_INET;
   serveraddress.sin_port=htons(8080);
   //serveraddress.sin_addr.s_addr=INADDR_ANY;
+  
+  
   //serveraddress.sin_addr.s_addr=inet_addr("192.168.1.121");
-  serveraddress.sin_addr.s_addr=inet_addr("127.0.0.1");
+  serveraddress.sin_addr.s_addr=inet_addr(arr);//Dynamically get ipaddress from txt file
+  
   
    //int connect(int sockfd, const struct sockaddr *addr,socklen_t addrlen);
   connect(sock_fd,(struct sockaddr *)&serveraddress,sizeof(serveraddress));
@@ -35,12 +62,14 @@ int main()
   char serverresponse[1024];
   recv(sock_fd,&serverresponse,sizeof(serverresponse),0);
   printf("server sent data : %s\n",serverresponse);
-  
   close(c_fd);
-  
+  close(fd);
   
 
 }
+
+
+
 
 
 
